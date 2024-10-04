f830c6fef0b61168cd551c0f889b95386e64c610
a::ir <108822895+debianchef@users.noreply.github.com>
2024-10-04 11:47:26 +0000

    main : We can start with the sign In from this base

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// use NODE_ENV to not have to change config based on where it's deployed
+export const NEXT_PUBLIC_URL = 'http://localhost:3000';
+// Add your API KEY from the Coinbase Developer Portal
+export const NEXT_PUBLIC_CDP_API_KEY = process.env.NEXT_PUBLIC_CDP_API_KEY;
+export const NEXT_PUBLIC_WC_PROJECT_ID = process.env.NEXT_PUBLIC_WC_PROJECT_ID;
