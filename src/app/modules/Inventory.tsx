8bfda8ac9613b0dba5ce89263f08c93f37a29318
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2024-10-06 01:06:35 +0100

    modules

---
+++ src/app/modules/Inventory.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Inventory = () => {
+  return (
+    <>
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+    <div className="text-xl">Inventory fick</div>;
+    </div>
+  </>  )
+}
+
+export default Inventory
